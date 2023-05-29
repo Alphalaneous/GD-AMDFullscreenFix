7e8ffacc391bebd311cd63ea0c8bc8668bc7e430
Ashton <38200084+Alphatism@users.noreply.github.com>
2023-05-29 19:56:41 -0400

    Initial commit

---
+++ src/dllmain.cpp
@@ -0,0 +1,99 @@
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#pragma warning(push, 0)
+#include <cocos2d.h>
+#pragma warning(pop)
+#include <MinHook.h>
+#include <gd.h>
+#include <chrono>
+#include <thread>
+#include <random>
+#include <CCGL.h>
+
+LONG_PTR oWindowProc;
+bool newWindowProcSet = false;
+
+int width;
+int height;
+
+LRESULT CALLBACK nWindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
+    switch (msg) {
+
+    case WM_SETFOCUS: {
+
+        SetWindowPos(hwnd, HWND_TOPMOST, 0, 0, width, height, SWP_DRAWFRAME | SWP_FRAMECHANGED);
+        break;
+    }
+    case WM_KILLFOCUS:
+        
+        RECT rect;
+        if (GetWindowRect(hwnd, &rect))
+        {
+            width = rect.right - rect.left;
+            height = rect.bottom - rect.top;
+        }
+
+        cocos2d::CCEGLView* ccEGLView = cocos2d::CCEGLView::sharedOpenGLView();
+
+        if (ccEGLView->isFullscreen()) {
+            SetWindowPos(hwnd, HWND_BOTTOM, 0, 0, 1280, 720, SWP_NOACTIVATE);
+        }
+
+        break;
+    }
+
+    return CallWindowProc((WNDPROC)oWindowProc, hwnd, msg, wparam, lparam);
+}
+
+bool(__thiscall* MenuLayer_init)(gd::MenuLayer* self);
+
+bool __fastcall MenuLayer_init_H(gd::MenuLayer* self, void*) {
+
+    if (!MenuLayer_init(self)) return false;
+
+
+
+    if (!newWindowProcSet) oWindowProc = SetWindowLongPtrA(GetForegroundWindow(), GWL_WNDPROC, (LONG_PTR)nWindowProc);
+    newWindowProcSet = true;
+
+    return true;
+}
+
+
+DWORD WINAPI thread_func(void* hModule) {
+    MH_Initialize();
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distr(100, 2000);
+
+    int random = distr(gen);
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(random));
+
+    /*AllocConsole();
+    freopen("CONIN$", "r", stdin);
+    freopen("CONOUT$", "w", stdout);
+    freopen("CONOUT$", "w", stderr);*/
+
+    auto base = reinterpret_cast<uintptr_t>(GetModuleHandle(0));
+   
+    MH_CreateHook(
+        reinterpret_cast<void*>(base + 0x1907B0),
+        MenuLayer_init_H,
+        reinterpret_cast<void**>(&MenuLayer_init)
+    );
+
+
+    MH_EnableHook(MH_ALL_HOOKS);
+    
+    return 0;
+}
+
+BOOL APIENTRY DllMain(HMODULE handle, DWORD reason, LPVOID reserved) {
+
+    if (reason == DLL_PROCESS_ATTACH) {
+        CreateThread(0, 0x100, thread_func, handle, 0, 0);
+    }
+    return TRUE;
+}
